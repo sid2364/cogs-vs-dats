31ef969fd98ded13285474f75450fa3cfcd05b63
Siddharth Sahay <sahaysid@gmail.com>
2017-11-27 12:31:26 +0530

    89% accuracy

--- conv_nn.py
+++ conv_nn.py
@@ -12,10 +12,10 @@ tf.reset_default_graph()
 
 image_dim = parse_data.image_dim
 training_data, testing_data = parse_data.get_data()
-model_path = '9.cogs-and-dats.model'
+model_path = '11.cogs-and-dats.model'
 
 # Make training set and testing set from the entire labeled training data
-test_set = int(len(training_data)*0.80)
+test_set = int(len(training_data)*0.8)
 X = np.array([t[0] for t in training_data[:test_set]])
 Y = np.array([t[1] for t in training_data[:test_set]])
 X = X.reshape([-1, image_dim, image_dim, 1])
@@ -42,7 +42,7 @@ def make_model():
 	convnet = conv_2d(convnet, 256, 5, activation='relu')
 	convnet = max_pool_2d(convnet, 5)
 
-	convnet = conv_2d(convnet, 64, 5, activation='relu')
+	convnet = conv_2d(convnet, 128, 5, activation='relu')
 	convnet = max_pool_2d(convnet, 5)
 	
 	convnet = fully_connected(convnet, 1024, activation='relu')
@@ -57,29 +57,29 @@ def make_model():
 
 def fit_model(model, X, Y, test_x, test_y):
 	print("Fitting model with data.")
-	model.fit({'input': X}, {'targets': Y}, n_epoch=15, validation_set=({'input': test_x}, {'targets': test_y}), 
+	model.fit({'input': X}, {'targets': Y}, n_epoch=12, validation_set=({'input': test_x}, {'targets': test_y}), 
 		snapshot_step=500, show_metric=True, run_id='cats-vs-dogs')
-	model.save(os.path.join("./models", model_path))
+	model.save(model_path)
 
 	return model
 
 def get_model():
 	print("Getting the model.")
 	model = make_model()
-	if not os.path.exists(os.path.join('models', model_path + ".index")):
+	if not os.path.exists(model_path + ".index"):
 		print(os.path.join('models', model_path) + " does not exist!")
 		fit_model(model, X, Y, test_x, test_y)
 	else:
-		print(os.path.join('models', model_path) + " exists! Loading the model.")
-		model.load(os.path.join('model', model_path))
+		print(model_path + " exists! Loading the model.")
+		model.load(model_path)
 	return model
 
 def make_predictions(model, test_arr):
 	return model.predict(test_arr)
 
 if __name__ == "__main__":
-	mdl = get_model()
-	predicted_data = make_predictions(mdl, predict_this_X)
+	model = get_model()
+	predicted_data = make_predictions(model, predict_this_X)
 	final = []
 	for i in range(len(predicted_data)):
 		final.append([predict_this_X_id[i], predicted_data[i]])
